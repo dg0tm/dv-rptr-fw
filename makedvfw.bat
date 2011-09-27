0a993b1cceb87cec47651f84ab5b991f21ed9396
Jan Alte <dv-develop@do1fjn.de>
2011-09-27 09:14:01 +0000

    git-svn-id: svn://svn.code.sf.net/p/dv-rptr-fw/code/trunk@35 208d634f-0f76-4b8c-b329-1362b4586a7b

--- makedvfw.bat
+++ makedvfw.bat
@@ -2,7 +2,7 @@
 if %1!==! goto message
 for %%i in ("%1") do set fwname=%%~di%%~pi%%~ni
 avr32-objcopy -O binary %1 "%fwname%.bin"
-D:\Projekte\dfu-loader\build\bin2dvfw.exe -d R -f "%fwname%.bin" -n "%fwname%"
+.\bin2dvfw.exe -d R -f "%fwname%.bin" -n "%fwname%"
 goto ende
 :message
 echo Call "makedvfw.bat <name of elf file>"
