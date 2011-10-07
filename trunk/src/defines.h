c432bd81ad785e2ecb75374fc54cb9761f16904f
do1fjn <do1fjn@208d634f-0f76-4b8c-b329-1362b4586a7b>
2011-10-07 13:38:24 +0000

    V0.31a STA_CANDUPLEX_MASK > Bit6 in ConfigFlags indicates duplex mode possible

--- trunk/src/defines.h
+++ trunk/src/defines.h
@@ -27,7 +27,7 @@
 #ifndef DEFINES_H_
 #define DEFINES_H_
 
-#define FIRMWAREVERSION		0x0310
+#define FIRMWAREVERSION		0x0311
 
 // Version Vxx.yy (last digit is char ' ', a, b...)
 
@@ -39,7 +39,7 @@
  * Bugfixlevel   c - Increased with one or more bugfixes (at the same time)
  */
 
-#define RELEASE_STRING		"2011-10-06"	// Release Date
+#define RELEASE_STRING		"2011-10-07"	// Release Date
 
 
 #define VERSION_IDENT		"DV-RPTR R." RELEASE_STRING
