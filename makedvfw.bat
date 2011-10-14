560f1fb5157398b5ffcf7174aa444e5259a95500
Jan Alte <dv-develop@do1fjn.de>
2011-10-14 07:15:34 +0000

    Updated Windows(R) BAT file to build a fw file, hardware definition changed for new PTT transistor (NPN)
    git-svn-id: svn://svn.code.sf.net/p/dv-rptr-fw/code/trunk@43 208d634f-0f76-4b8c-b329-1362b4586a7b

--- makedvfw.bat
+++ makedvfw.bat
@@ -2,7 +2,7 @@
 if %1!==! goto message
 for %%i in ("%1") do set fwname=%%~di%%~pi%%~ni
 avr32-objcopy -O binary %1 "%fwname%.bin"
-.\bin2dvfw.exe -d R -f "%fwname%.bin" -n "%fwname%"
+..\bin2dvfw.exe -d R -f "%fwname%.bin" -n "%fwname%"
 goto ende
 :message
 echo Call "makedvfw.bat <name of elf file>"
