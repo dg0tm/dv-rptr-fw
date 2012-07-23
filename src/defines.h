61e09bfa8d1d189acca5d931850efa10e403faee
Jan Alte <dv-develop@do1fjn.de>
2012-07-23 16:07:45 +0000

    V1.67a is_pttactive() macro replaced, cause rptr_tx_status variable indicates the true PTT status -> Problems on PTT-lock fixed.
    git-svn-id: svn://svn.code.sf.net/p/dv-rptr-fw/code/trunk@87 208d634f-0f76-4b8c-b329-1362b4586a7b

--- src/defines.h
+++ src/defines.h
@@ -27,7 +27,7 @@
 #ifndef DEFINES_H_
 #define DEFINES_H_
 
-#define FIRMWAREVERSION		0x1670
+#define FIRMWAREVERSION		0x1671
 
 // Version Vxx.yy (last digit is char ' ', a, b...)
 
@@ -39,7 +39,7 @@
  * Bugfixlevel   c - Increased with one or more bugfixes (at the same time)
  */
 
-#define RELEASE_STRING		"2012-07-21"	// Release Date
+#define RELEASE_STRING		"2012-07-23"	// Release Date
 
 
 #define VERSION_IDENT		"DV-RPTR R." RELEASE_STRING
